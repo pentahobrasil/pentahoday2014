820fab388331df961b99899fc3a4d79a5bfd4764
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-01-24 14:07:11 -0200

    Pentaho Day 2014 Website

---
+++ PentahoDay_2014/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	$('#number4').delay(800).animate({opacity: 1},400);
+	$('#number5').delay(1300).animate({opacity: 1},400);
+	$('#number6').delay(1800).animate({opacity: 1},400);
+	$('#welcomePDF').delay(2300).animate({opacity: 1},400);
+	
+	if ($(window).scrollTop() > 175) {
+		$('#firstRegister').delay(2700).animate({opacity: 1},400);
+		$('#transmissao').delay(2300).animate({opacity: 1},400);
+	}
+	if ($(window).scrollTop() > 250) {
+		$('#secondRegister').delay(2300).animate({opacity: 1},400);
+	}
+	if ($(window).scrollTop() > 2400) {
+		$('#thirdRegister').delay(2300).animate({opacity: 1},400);
+	}
+});
+
+$(document).scroll(function() {
+
+	if ($(window).scrollTop() > 100) {
+		$('#welcomePDF').stop().animate({opacity: 1},400);
+	}
+	if ($(window).scrollTop() > 175) {
+		$('#firstRegister').delay(400).animate({opacity: 1},400);
+		$('#transmissao').animate({opacity: 1},400);
+	}
+	if ($(window).scrollTop() > 250) {
+		$('#secondRegister').animate({opacity: 1},400);
+	}
+	if ($(window).scrollTop() > 2400) {
+		$('#thirdRegister').animate({opacity: 1},400);
+	}
+}); 
